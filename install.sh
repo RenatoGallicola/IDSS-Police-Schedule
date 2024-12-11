0010610a882612143a790659438f1ff7cb4167c7
DPazienza <d.pazienza1@studenti.uniba.it>
2024-12-11 16:36:55 +0100

    update scritp install

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # Installation script for required Python packages
 
 # Update pip to the latest version
-python3 -m pip install --upgrade pip
+python -m pip install --upgrade pip
 
 # Install required Python packages
 pip install -r requirements.txt
